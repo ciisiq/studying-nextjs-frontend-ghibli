c4b31161a08739bac71ddd41f3ee93758435df67
ciisiq <cii_3@hotmail.com>
2023-05-03 19:20:55 +0100

    updating the image link

--- pages/films/[id].js
+++ pages/films/[id].js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const getStaticPaths = async () => {
   const res = await fetch("http://localhost:8080/api/films");
   const data = await res.json();
@@ -27,16 +29,18 @@ export const getStaticProps = async (context) => {
 
 const Details = ({ film }) => {
   return (
-    <div>
-      <h1 className="font-bold text-ft-black text-[42px] leading-none">
-        {film.title}
-      </h1>
-      <p>{film.genre}</p>
-      <p>{film.release}</p>
-      <p>{film.director}</p>
-      <p>{film.synopsis}</p>
-      <p>{film.poster}</p>
-    </div>
+    <>
+      <div className="bg-white">
+        <h1 className="font-bold text-ft-black text-[42px] leading-none">
+          {film.title}
+        </h1>
+        <p>{film.genre}</p>
+        <p>{film.release}</p>
+        <p>{film.director}</p>
+        <p>{film.synopsis}</p>
+        <Image src={film.poster} alt="film poster" width={160} height={150} />
+      </div>
+    </>
   );
 };
 
